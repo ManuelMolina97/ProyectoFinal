d02f5ac899f1730abcf1830b6c1d6ffc9bacf529
CristoArana <cristo_arana@hotmail.es>
2016-05-24 21:39:31 +0200

    Segunda versión del inde.php, refactorirando el backend

--- index.php
+++ index.php
@@ -2,27 +2,46 @@
         <?php
         require './vendor/autoload.php';
         
-        /*init_set('error_reporting', E_ALL ^ E_NOTICE);
-        init_set('display_errors','on');*/
-        
         if ($_SERVER["REQUEST_METHOD"] === "GET"){
-            require __DIR__.'/index.html';
-            
+            require __DIR__.'/Vista.php';
+            $vista = new ProyectoFinal\Vista();
+            $vista->mostrarPagina("index.html");
             
         } else {
             $queryJSON = file_get_contents('php://input');
-            $historia = json_decode($queryJSON, true);
+            $consulta = json_decode($queryJSON, true);
             
-            $db = new \mysqli("localhost","cristo","cristo","scrum");
-            $sql = "INSERT INTO historiausuario VALUES ('".$historia["id"]."','".$historia["descripcion"]."','".$historia["valor"]."');";
-            $resultadoInsercion = $db->query($sql);
+            require __DIR__."/Modelo.php";
+            require __DIR__."Vista.php";
+            $modelo = new ProyectoFinal\Modelo();
+            $vista = new ProyectoFinal\Vista();
             
-            if ($resultadoInsercion){
-                echo json_encode($historia); //ESTO LO HE CAMBIADO, PONIA $queryJSON, Y NO CUADRABA ESO CON EL PROYECTO DE PEQUE
-            } else {
-                echo json_encode(["error" => "No se ha podido insertar"]);
+            $modelo->conexion();
+            
+            if(strcmp($consulta["op"], "insertar") == 0){
+                $resultado = $modelo->crearHistoria($consulta);           
+                if ($resultado){
+                    $vista->mostrarDatos($consulta);
+                } else {
+                    $vista->enviarError($consulta["op"], "insertar");
+                }
+            
+            }else if (strcmp($consulta["op"], "borrar") == 0){
+                $resultado = $modelo->borrarHistoria($consulta);
+                if($resultado){
+                    $vista->mostrarDatos($consulta);
+                } else {
+                    $vista->enviarError($consulta["op"], "borrar");
+                }
+                
+            }else if (strcmp($consulta["op"], "modificar") == 0){
+                $resultado = $modelo->modificarHistoria($consulta);
+                if($resultado){
+                    $vista->mostrarDatos($consulta);
+                } else {
+                    $vista->enviarError($consulta["op"], "modificar");
+                }
             }
-           /*$vista = new proyectoMauri\Vista();
-           $vista->mostrarPagina("index.html");*/
+          
         }
 
